afd439eed934b6e84d8fc6bb9051b2d3aad980cd
Juanchi Novas <juanchinivas@gmail.com>
2021-01-15 23:53:54 -0400

    last touches and mobile version

--- core/js/core.js
+++ core/js/core.js
@@ -1,53 +1,55 @@
 
-const hexToRgb = hex => {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-    } : null
-}
-
-function isDarkColor(hexColor) {
-
-    const { r, g, b } = hexToRgb(hexColor)
-
-    let colorArray = [r / 255, g / 255, b / 255].map(v => {
-        if (v <= 0.03928) {
-            return v / 12.92
-        }
-
-        return Math.pow((v + 0.055) / 1.055, 2.4)
-    })
-
-    const luminance = 0.2126 * colorArray[0] + 0.7152 * colorArray[1] + 0.0722 * colorArray[2]
-
-    return luminance <= 0.179
-}
-
-function debounce(func, wait) {
-    let timerId = null;
-
-    return function debounced() {
-        let context = this, args = arguments;
-		let later = function() {
-			// timerId = null;
-			func.apply(context, args);
-            clearTimeout(timerId);
-		};
-		clearTimeout(timerId);
-		timerId = setTimeout(later, wait);
-    }
-}
 
 const core = (function () {
     const innerState = {
         fgColor: "#000000",
         bgColor: "#FFFFFF",
-        chosenColors: ["#000000"],
-        lineSize: 1
+        chosenColors: ["#000000", "#ff0000", "#ff00ea", "#1e00ff", "#0091ff", "#00ffee", "#44ff00"],
+        lineSize: 1,
+        isErasing: false
     };
+
+    const hexToRgb = hex => {
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+    
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16),
+        } : null
+    }
+    
+    function isDarkColor(hexColor) {
+    
+        const { r, g, b } = hexToRgb(hexColor)
+    
+        let colorArray = [r / 255, g / 255, b / 255].map(v => {
+            if (v <= 0.03928) {
+                return v / 12.92
+            }
+    
+            return Math.pow((v + 0.055) / 1.055, 2.4)
+        })
+    
+        const luminance = 0.2126 * colorArray[0] + 0.7152 * colorArray[1] + 0.0722 * colorArray[2]
+    
+        return luminance <= 0.179
+    }
+    
+    function debounce(func, wait) {
+        let timerId = null;
+    
+        return function debounced() {
+            let context = this, args = arguments;
+            let later = function() {
+                // timerId = null;
+                func.apply(context, args);
+                clearTimeout(timerId);
+            };
+            clearTimeout(timerId);
+            timerId = setTimeout(later, wait);
+        }
+    }
 
     function onColorFgChange(color) {
         markColorAsChosen(color);
@@ -60,7 +62,7 @@ const core = (function () {
 
     function markColorAsChosen(color) {
         const colorIndex = innerState.chosenColors.findIndex(c => c === color);
-        if (colorIndex < 0/* && innerState.chosenColors.length < 20*/) {
+        if (colorIndex < 0 && innerState.chosenColors.length < 21) {
             innerState.chosenColors.push(color);
         }
     }
@@ -73,6 +75,15 @@ const core = (function () {
         };
     }
 
+    function reduce(fn, element) {
+        let found = false;
+        if (fn && !found) {
+            found = fn(element);
+        }
+
+        return found && element || reduce(fn, element.parentNode);
+    }
+
     function formToObject(form) {
         const len = form.elements.length;
         const objForm = {};
@@ -80,11 +91,121 @@ const core = (function () {
         const value = (value) => (value==="" || isNaN(+value))? value: +value;
         for (let i = 0; i < len; i++) {
             const element = form.elements[i];
-            if (element.tagName === "BUTTON") continue;
+            // if (element.tagName === "BUTTON") continue;
             objForm[name(element.name || element.id)] = value(element.value);
         }
 
         return objForm;
+    }
+
+    function downloadDraw(event) {
+        event.preventDefault();
+
+        var aLink = document.querySelector('a#download');
+        aLink.download = 'draw.png';
+        aLink.href = canvas.toDataURL();
+        aLink.click();
+
+        return false;
+    }
+
+    function setCanvas(canvas, hideContextualMenu, contextMenu) {
+        // const { width: parentWidth, height: parentHeight } = getComputedStyle(canvas.parentNode);
+        const ctx = canvas.getContext("2d");
+        let onAction = false;
+
+        // canvas.width = parseFloat(parentWidth);
+        // canvas.height = parseFloat(parentHeight);
+        onResize();
+
+
+        function mouseDown(event) {
+            if (event.target !== canvas) return;
+
+            hideContextualMenu();
+            const mousePosition = getMousePosition(canvas, event);
+            
+            ctx.strokeStyle = (innerState.isErasing && innerState.bgColor) || innerState.fgColor;
+            ctx.lineWidth = innerState.lineSize;
+
+            ctx.beginPath();
+            ctx.moveTo(mousePosition.x, mousePosition.y);
+
+            onAction = true;
+        };
+
+        function mouseUp(event) {
+            const mousePosition = getMousePosition(canvas, event);
+
+            ctx.moveTo(mousePosition.x, mousePosition.y);
+            ctx.closePath();
+
+            onAction = false;
+        };
+
+        function mouseMove(event) {
+            if (onAction) {
+                const mousePosition = getMousePosition(canvas, event);
+                ctx.lineTo(mousePosition.x, mousePosition.y);
+                ctx.stroke();
+            }
+        };
+
+        function setContentMenu(event) {
+            event.preventDefault();
+
+            contextMenu.style.top = `${event.clientY}px`;
+            contextMenu.style.left = `${event.clientX}px`;
+            contextMenu.classList.remove("hide");
+
+        }
+
+        function setCanvasBgColors(bgColor) {
+            const { offsetHeight, offsetWidth } = canvas;
+            ctx.fillStyle = bgColor;
+            ctx.fillRect(0, 0, offsetWidth, offsetHeight);
+    
+        }
+    
+        function addTextToCanvas(opcion) {
+            ctx.font = `${opcion.textFontSize}px ${opcion.textFont}`;
+            
+            if (opcion.textStyle === "fill") {
+                ctx.fillStyle = opcion.textColor;
+                ctx.fillText(opcion.textField, opcion.textFontCoordinateX, opcion.textFontCoordinateY);
+            } else {
+                ctx.strokeStyle = opcion.textColor;
+                ctx.strokeText(opcion.textField, opcion.textFontCoordinateX, opcion.textFontCoordinateY);
+            }
+        }
+    
+        function addImageToCanvas(opcion) {
+            ctx.drawImage(opcion.img, opcion.imageCoordinateX, opcion.imageCoordinateY, +opcion.imgWidth, +opcion.imgHeight);
+        }
+
+        function onResize() {
+            const { width: parentWidth, height: parentHeight } = getComputedStyle(canvas.parentNode);
+    
+            canvas.width = parseFloat(parentWidth);
+            canvas.height = parseFloat(parentHeight);
+
+            console.log(parentWidth, parentHeight);
+        }
+
+        canvas.addEventListener("contextmenu", setContentMenu, false);
+        document.addEventListener("resize", onResize, false);
+
+        return {
+            mouseDown,
+            mouseUp,
+            mouseMove,
+            setCanvasBgColors,
+            addTextToCanvas,
+            addImageToCanvas,
+            removeContextMenuListerner: () => canvas.removeEventListener("contextmenu", setContentMenu),
+            removeResizeListerner: () => document.removeEventListener("resize", onResize)
+        }
+
     }
 
     return {
@@ -94,7 +215,10 @@ const core = (function () {
         formToObject,
         debounce,
         isDarkColor,
-        state: () => innerState
+        state: () => innerState,
+        reduce,
+        downloadDraw,
+        setCanvas
     }
 })();
 
---
+++ views/vanilla-front/vanilla.js
@@ -0,0 +1,203 @@
+
+window.onload = function () {
+    const painteer = window.painteer;
+
+    const canvas = document.querySelector(`#canvas`);
+    const canvasFns = painteer.setCanvas(canvas, hideContextualMenu, document.querySelector("#contextual-menu"));
+
+    document.addEventListener("mousedown", canvasFns.mouseDown, false);
+    document.addEventListener("mouseup", canvasFns.mouseUp, false);
+    document.addEventListener("mousemove", canvasFns.mouseMove, false);
+
+    const fgColorPicker = document.querySelector("#fg-color-picker");
+    fgColorPicker.value = painteer.state().fgColor;
+    changeColor({ target: fgColorPicker }, fgColorPicker.parentNode, painteer.onColorFgChange);
+
+    const triggerFunc = painteer.debounce((event) => {
+        changeColor(event, fgColorPicker.parentNode, painteer.onColorFgChange);
+        setChosenColors(painteer.state().chosenColors);
+    }, 250);
+    fgColorPicker.addEventListener("input", triggerFunc, false);
+
+    const bgColorPicker = document.querySelector("#bg-color-picker");
+    bgColorPicker.value = painteer.state().bgColor;
+    changeColor({ target: bgColorPicker }, bgColorPicker.parentNode, painteer.onColorBgChange);
+    bgColorPicker.addEventListener("input", (event) => {
+        changeColor(event, bgColorPicker.parentNode, painteer.onColorBgChange);
+    }, false);
+
+
+    setChosenColors(painteer.state().chosenColors);
+
+    document.querySelector("form#dialog-form-add-text").onsubmit = function (event) {
+        event.preventDefault();
+
+        const form = painteer.formToObject(event.target);
+        canvasFns.addTextToCanvas(form);
+
+        const parent = painteer.reduce(function(el) {
+            return el.getAttribute("role") ==='dialog';
+        }, this.parentElement);
+
+        parent.classList.add("hide");
+
+        return false;
+    };
+
+    document.querySelector("form#dialog-form-add-image").onsubmit = function (event) {
+        event.preventDefault();
+
+        const form = painteer.formToObject(event.target);
+        form.img = this.querySelector("img#img-selected");
+        form.img.width = +form.imgWidth;
+        form.img.height = +form.imgHeight;
+
+        canvasFns.addImageToCanvas(form);
+
+        const parent = painteer.reduce(function(el) {
+            return el.getAttribute("role") ==='dialog';
+        }, this.parentElement);
+
+        parent.classList.add("hide");
+
+        return false;
+    };
+
+    document.querySelector("#contextual-menu ul li#add-text").
+        addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const dialog = document.querySelector("#text-add-dialog");
+            const mousePosition = painteer.getMousePosition(canvas, event);
+
+            dialog.querySelector("#text-font-coordinate-x").value = mousePosition.x;
+            dialog.querySelector("#text-font-coordinate-y").value = mousePosition.y;
+
+            dialog.classList.remove("hide");
+            hideContextualMenu();
+
+            return false;
+        }, false);
+
+    document.querySelector("#contextual-menu ul li#add-img").
+        addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const dialog = document.querySelector("#image-add-dialog");
+            const mousePosition = painteer.getMousePosition(canvas, event);
+
+            dialog.querySelector("#image-coordinate-x").value = mousePosition.x;
+            dialog.querySelector("#image-coordinate-y").value = mousePosition.y;
+
+            dialog.querySelector("#img-width").value = 100;
+            dialog.querySelector("#img-height").value = 100;
+
+            dialog.querySelector("#image-field")
+            .addEventListener('change', function(event) {
+                let file = event.target.files[0];
+
+                let reader = new FileReader();
+
+                reader.readAsDataURL(file);
+
+                reader.onload = function() {
+                    dialog.querySelector("#img-selected").src = reader.result;
+                };
+            }, false);
+
+            dialog.classList.remove("hide");
+            hideContextualMenu();
+
+            return false;
+        }, false);
+
+    document.querySelectorAll("#text-add-dialog #btn-close, #image-add-dialog #btn-close").
+    forEach(el => el.addEventListener("click", closeDialog , false));
+
+    const sizes = document.querySelector("#size-container #size__options-lst");
+    sizes.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        painteer.state().lineSize = +event.target.textContent;
+        this.parentNode.querySelector("#size__label").textContent = painteer.state().lineSize;
+
+        return false;
+    }, false);
+
+    const eraser = document.querySelector("#eraser-container");
+    eraser.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        if (this.classList.contains("active")) {
+            this.classList.remove("active");
+            painteer.state().isErasing = false;
+
+            return;
+        }
+
+        this.classList.add("active");
+        painteer.state().isErasing = true;
+        return false;
+    }, false);
+
+    const download = document.querySelector("#image-container");
+    download.addEventListener("click", painteer.downloadDraw, false);
+
+    function changeColor(event, parentPicker, fn) {
+        parentPicker.style.backgroundColor = event.target.value;
+        parentPicker.style.color = painteer.isDarkColor(event.target.value) ? "white" : "black";
+        fn && fn(event.target.value);
+
+        if (fn === painteer.onColorBgChange) {
+            canvasFns.setCanvasBgColors(event.target.value);
+        }
+    }
+
+    function setChosenColors(chosenColors) {
+        const chosenColor = document.querySelector(`.color-picker-chosen #lst-color-chosen`);
+        for (let i = 0, len = chosenColors.length; i < len; i++) {
+            const span = chosenColor.querySelector(`#chosen-${i}`) || document.createElement("span");
+            span.id = `chosen-${i}`;
+            span.role = `button`;
+            span.setAttribute("data-color", chosenColors[i]);
+            span.setAttribute("title", chosenColors[i]);
+            span.setAttribute("aria-lable", `Color ${chosenColors[i]}`);
+            span.setAttribute("tabindex", i);
+            span.style.backgroundColor = chosenColors[i];
+
+            if (!chosenColor.querySelector(`#chosen-${i}`)) {
+                span.addEventListener("click", (event) => {
+                    painteer.onColorFgChange(event.target.dataset.color);
+                    changeColor({ 
+                        target: { 
+                            value: event.target.dataset.color
+                        } 
+                    }, fgColorPicker.parentNode, painteer.onColorFgChange);
+                }, false);
+
+                const li = document.createElement("li");
+                li.id = `li-chosen-${i}`;
+                li.appendChild(span);
+                chosenColor.appendChild(li);
+
+            }
+        }
+    }
+
+    function closeDialog(event) {
+        event.preventDefault();
+        
+        const parent = painteer.reduce(function(el) {
+            return el.getAttribute("role") ==='dialog';
+        }, event.target.parentNode);
+
+        parent.classList.add("hide");
+        
+        return false;
+    }
+
+    function hideContextualMenu() {
+        document.querySelector("#contextual-menu").classList.add("hide");
+    }
+
+}
